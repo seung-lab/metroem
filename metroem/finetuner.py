d71ad086bc73fa5cd79069d549408e5c248ee49a
Nico Kemnitz <nkemnitz@princeton.edu>
2021-10-13 16:00:30 -0400

    performance(finetuner): Remove NaN-related code paths

--- metroem/finetuner.py
+++ metroem/finetuner.py
@@ -82,7 +82,6 @@ def optimize_pre_post_ups(src, tgt, initial_res, sm, lr, num_iter,
     best_loss = loss_dict['result'].cpu().detach().numpy()
     new_best_ago = 0
     lr_halfed_count = 0
-    nan_count = 0
     no_impr_count = 0
     new_best_count = 0
     if verbose:
@@ -96,53 +95,35 @@ def optimize_pre_post_ups(src, tgt, initial_res, sm, lr, num_iter,
         loss_var += (loss_bundle['pred_res']**2).mean() * l2
         curr_loss = loss_var.cpu().detach().numpy()
 
-        if np.isnan(curr_loss):
-            nan_count += 1
-            lr /= 1.5
-            lr_halfed_count += 1
-            pre_res = prev_pre_res.clone().detach()
-            post_res = prev_post_res.clone().detach()
-            post_res.requires_grad = True
-            pre_res.requires_grad = True
-            trainable = [pre_res, post_res]
-            if opt_mode == 'adam':
-                optimizer = torch.optim.Adam([pre_res, post_res], lr=lr, weight_decay=wd)
-            elif opt_mode == 'sgd':
-                optimizer = torch.optim.SGD([pre_res, post_res], lr=lr, **opt_params)
-            prev_loss = []
+        min_improve = 1e-11
+        if curr_loss + min_improve <= best_loss:
+            # Improvement
+            prev_pre_res = pre_res.clone()
+            prev_post_res = post_res.clone()
+            best_loss = curr_loss
+            new_best_count += 1
             new_best_ago = 0
         else:
-            min_improve = 1e-11
-            if curr_loss + min_improve <= best_loss:
-                # Improvement
-                prev_pre_res = pre_res.clone()
-                prev_post_res = post_res.clone()
-                best_loss = curr_loss
-                new_best_count += 1
-                new_best_ago = 0
-            else:
-                new_best_ago += 1
-                if new_best_ago > noimpr_period:
-                    # No improvement, reduce learning rate
-                    no_impr_count += 1
-                    lr /= 2
-                    lr_halfed_count += 1
+            new_best_ago += 1
+            if new_best_ago > noimpr_period:
+                # No improvement, reduce learning rate
+                no_impr_count += 1
+                lr /= 2
+                lr_halfed_count += 1
 
-                    if opt_mode == 'adam':
-                        optimizer = torch.optim.Adam([pre_res, post_res], lr=lr)
-                    elif opt_mode == 'sgd':
-                        optimizer = torch.optim.SGD([pre_res, post_res], lr=lr, **opt_params)
-                    new_best_ago -= 5
-                prev_loss.append(curr_loss)
+                if opt_mode == 'adam':
+                    optimizer = torch.optim.Adam([pre_res, post_res], lr=lr)
+                elif opt_mode == 'sgd':
+                    optimizer = torch.optim.SGD([pre_res, post_res], lr=lr, **opt_params)
+                new_best_ago -= 5
+            prev_loss.append(curr_loss)
 
-            optimizer.zero_grad()
-            loss_var.backward()
-            pre_res.grad[pre_res.grad != pre_res.grad] = 0
-            post_res.grad[post_res.grad != post_res.grad] = 0
-            optimizer.step()
+        optimizer.zero_grad()
+        loss_var.backward()
+        optimizer.step()
 
-            if lr_halfed_count >= max_bad or nan_count > max_bad:
-                break
+        if lr_halfed_count >= max_bad:
+            break
 
     loss_bundle['pred_res'] = combine_pre_post(pred_res, prev_pre_res, prev_post_res).up(opt_res_coarsness)
     loss_bundle['pred_tgt'] = loss_bundle['pred_res'].from_pixels()(src)
@@ -150,7 +131,7 @@ def optimize_pre_post_ups(src, tgt, initial_res, sm, lr, num_iter,
 
     e = time.time()
     if verbose:
-        print ("New best: {}, No impr: {}, NaN: {}, Iter: {}".format(new_best_count, no_impr_count, nan_count, epoch))
+        print ("New best: {}, No impr: {}, Iter: {}".format(new_best_count, no_impr_count, epoch))
         print (loss_dict['result'].cpu().detach().numpy(), loss_dict['similarity'].detach().cpu().numpy(), loss_dict['smoothness'].detach().cpu().numpy())
         print (e - s)
         print ('==========')
